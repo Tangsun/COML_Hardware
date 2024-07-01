bd9f9d17a47730245bce752cbb80bab6508fa9e4
adenm <aden.mckinney10@gmail.com>
2024-07-01 15:09:41 -0400

    put train_many_multi to replicate thesis results

--- COML_hardware/train_many_multi.sh
+++ COML_hardware/train_many_multi.sh
@@ -4,6 +4,8 @@
 #SBATCH --nodes=N                           # Number of nodes
 #SBATCH --ntasks=N                          # Number of tasks (processes)
 #SBATCH --cpus-per-task=1                   # Number of CPU cores per task
+# I think we can speed this up by increasing this to the max amount of cpus in a node
+# Believe number of nodes and number of tasks is automatically allocated
 #SBATCH -o %j.log                           # Standard output and error log
 
 meta_epochs=1000
@@ -14,22 +16,22 @@ do
     do
         for p in 2.0
         do
-            for p_freq in 2000
+            for p_freq in 2000 # 25 50 100 250 2000
             do
-                for reg_P in 1e-1
+                for reg_P in 1 # 1e-1
                     do
-                    for reg_k_R in 0
+                    for reg_k_R in 0.001 # 0
                     do
-                        for k_R_scale in 1
+                        for k_R_scale in 1 # 1
                         do
-                            for k_R_z in 1.4
+                            for k_R_z in 1.26 #1.4 
                             do
                                 echo -e "\n============================="
                                 echo -e "seed = $seed\nM = $M\npnorm_init = $p\np_freq = $p_freq\nmeta_epochs = 1000"
                                 echo -e "reg_P = $reg_P\nreg_k_R = $reg_k_R\nk_R_scale = $k_R_scale\nk_R_z = $k_R_z"
                                 echo -e "output_dir = reg_P_${reg_P}_reg_k_R_${reg_k_R}_k_R_scale_${k_R_scale}_k_R_z_${k_R_z}_z_training"
                                 echo -e "=============================\n"
-                                srun --ntasks=1 ./train_single.sh $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs $meta_epochs --reg_P $reg_P --reg_k_R $reg_k_R --k_R_scale $k_R_scale --k_R_z $k_R_z --output_dir "reg_P_${reg_P}_reg_k_R_${reg_k_R}_k_R_scale_${k_R_scale}_k_R_z_${k_R_z}_z_training"
+                                srun --exclusive --ntasks=1 ./train_single.sh $seed $M --pnorm_init $p --p_freq $p_freq --meta_epochs $meta_epochs --reg_P $reg_P --reg_k_R $reg_k_R --k_R_scale $k_R_scale --k_R_z $k_R_z --output_dir "reg_P_${reg_P}_reg_k_R_${reg_k_R}_k_R_scale_${k_R_scale}_k_R_z_${k_R_z}_z_training"
                             done
                         done
                     done
